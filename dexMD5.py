e1f07d0dec47c4f9c07b30fc90ae79a25730854b
mgp25 <me@mgp25.com>
2017-08-06 17:59:42 +0200

    Move to folders, more organized

--- dexMD5.py
+++ dexMD5.py
@@ -3,9 +3,11 @@ import hashlib
 import base64
 
 apkName = "WA.apk"
-zipFile = zipfile.ZipFile(apkName,'r')
+try:
+    zipFile = zipfile.ZipFile(apkName,'r')
+    hash = hashlib.md5()
+    hash.update(zipFile.read('classes.dex'))
 
-hash = hashlib.md5()
-hash.update(zipFile.read('classes.dex'))
-
-print("ClassedDEX: "+ base64.b64encode(hash.digest()));
+    print("ClassesDEX: "+ base64.b64encode(hash.digest()));
+except Exception:
+    print("WA.apk not found.")
