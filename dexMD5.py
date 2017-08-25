e8ba07e3db10a674201abf881df1c0e33327465d
mgp25 <me@mgp25.com>
2017-08-25 16:56:07 +0200

    Update python script

--- dexMD5.py
+++ dexMD5.py
@@ -1,13 +1,22 @@
+import sys
+import base64
 import zipfile
 import hashlib
-import base64
 
-apkName = "WA.apk"
+if len(sys.argv) < 2:
+    print("Usage: python dexMD5.py <file.apk>")
+    exit()
+else:
+    apkFile = sys.argv[1]
 try:
-    zipFile = zipfile.ZipFile(apkName,'r')
+    zipFile = zipfile.ZipFile(apkFile,'r')
+    classesDexFile = zipFile.read('classes.dex')
     hash = hashlib.md5()
-    hash.update(zipFile.read('classes.dex'))
+    hash.update(classesDexFile)
 
-    print("ClassesDEX: "+ base64.b64encode(hash.digest()));
-except Exception:
-    print("WA.apk not found.")
+    version = classesDexFile.decode("utf-8", errors = 'ignore').partition('App: ')[-1].partition(',')[0]
+
+    print("Version: " + version)
+    print("ClassesDex: " + base64.b64encode(hash.digest()).decode("utf-8"));
+except Exception as e:
+    print(sys.argv[1] + " not found.")
